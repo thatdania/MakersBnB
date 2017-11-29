6c1add8604e04d7802fcd0baf1b4dc91c1c8aae3
Lewis <lewisyoul@gmail.com>
2017-11-29 11:14:31 +0000

    get request from rental_overview to display rental properties

---
+++ app/public/src/rental_overview.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $.get('/rental/current_view', function(rental){
+    $('#rental-overview').append("<div class='name'>"+ rental.name +"</div>")
+    $("#rental-overview").append("<div class='location'>" + rental.location + "</div>")
+    $("#rental-overview").append("<div class='price'>" + rental.price + "</div>")
+    $("#rental-overview").append("<div class='capacity'>" + rental.capacity + "</div>")
+    $("#rental-overview").append("<form class='bookButton' action='' method=''><button id='more-info' type='submit' name='' value=''>Book</button></form>")
+  });
+
+
+});
